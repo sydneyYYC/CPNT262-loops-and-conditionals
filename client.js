037ebc204f18e4ffcdcde1946ba5a2ddd027c7a6
Sydney Bruce <sydneybruce20@gmail.com>
2023-10-23 21:09:59 -0700

    reverse a string and rendering the capitalized string

--- client.js
+++ client.js
@@ -4,16 +4,21 @@
 
 // have a name in an array with two words
 const jelly = "box jellyfish";
-
+console.log(jelly);
 
 //call function
 function capitalizeFirstLetter(str) {
   //will map across each and uppercase the first character and add the rest
   //this one does all of the things from attempt 2 but uses word differently and combines them alk into one line of code WOW 
-  return str.split(" ").map((word) =>word[0].toUpperCase() + word.slice(1).join(" "));
+  return str.split(" ").map((word) => word[0].toUpperCase() + word.slice(1).join(" "));
 }
 //execute / invoke function
-capitalizeFirstLetter(jelly);
+let capitalJelly= capitalizeFirstLetter(jelly);
+//render on page
+document.querySelector("#capital-slot").innerHTML = capitalJelly;
+
+document.querySelector("#jelly-spot") .innerHTML = jelly;
+
 
 // so this gives us an uppercase first letter of each but the problem is it returns undefined with the first letter
 // for (let i = 0; i < newArr.length; i++) {
@@ -33,4 +38,28 @@ capitalizeFirstLetter(jelly);
 //join words with a space
 // const capitalizeJelly = newArr.join(" ");
 
-// problem 2 
+
+
+// problem 2 reverse a string
+
+// 1. make a string that will hold the value of the return string
+// function reverseAString(str) {
+
+//   let revStr = "";
+
+//   // step two break the string in a conditional loop that will start at -1 
+//   // the string begins at -1 - the last letter and will decrease through all letters 
+//   for (let i = str.length - 1; i >= 0; i--)
+//   // the reverse string is the string plus its reversed self
+//   newString = newString + str[i];
+//   return newString;
+// }
+
+// reverseAString("why god");
+
+
+function reverseString(str) {
+  return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+  document.querySelector("#reverse-string").innerHTML = this;
+}
+reverseString("hello");
