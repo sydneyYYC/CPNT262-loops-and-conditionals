60b25cb1b0dbc6bb2a4003691a1ced1637df979b
Sydney Bruce <sydneybruce20@gmail.com>
2023-10-24 13:48:03 -0700

    cleaned up code and added comments to js

--- README.md
+++ README.md
@@ -4,3 +4,23 @@
 
 
 ### Attributions
+[guides](https://developer.mozilla.org/en-US/)
+
+
+
+### Code Journal
+
+Had lots of errors and debugging with the first problem
+ capitalization problems
+ so this gives us an uppercase first letter of each but the problem is it returns undefined with the first letter
+// for (let i = 0; i < newArr.length; i++) {
+//   newArr[i] = newArr[i][0].toUpperCase();
+//   console.log(newArr);
+
+returns "Bundefined, Jundefined"
+
+this doesnt join the words correctly - nor does it use MAP correctly which was throwing errors
+//split into two words and invoke the function across all words
+// const newArr = jelly.split(" ").map(capitalizeFirstLetter);
+//join words with a space
+// const capitalizeJelly = newArr.join(" ");
--- client.js
+++ client.js
@@ -1,70 +1,35 @@
 // Title Case a sentence (all words get capitalized for their first letter)
-// splice the two together
-// im gunna use jellyfish cause i think theyre cute :> hehe
 
 // have a name in an array with two words
 const jelly = "box jellyfish";
-console.log(jelly);
+let jellyCRen = document.querySelector("#capital-slot")
+let jellyRRen = document.querySelector("#jelly-spot")
 
 //call function
 function capitalizeFirstLetter(str) {
-  //will map across each and uppercase the first character and add the rest of the characters
+  //will map across each starting at the first and uppercase the first character and add the rest of the characters
   let newStr = str.split(" ").map((word) => word[0].toUpperCase() + word.slice(1));
   // return the capitalized strings and join them with a space
   return newStr.join(" ");
 }
-//execute / invoke function
+//execute / invoke capitalize function
 let capitalJelly= capitalizeFirstLetter(jelly);
 
 //render on page
-document.querySelector("#capital-slot").innerHTML = capitalJelly;
+jellyCRen.innerHTML = capitalJelly;
 //original string for comparison
-document.querySelector("#jelly-spot") .innerHTML = jelly;
+jellyRRen.innerHTML = jelly;
 
+// reverse a string problem
 
-// so this gives us an uppercase first letter of each but the problem is it returns undefined with the first letter
-// for (let i = 0; i < newArr.length; i++) {
-//   newArr[i] = newArr[i][0].toUpperCase();
-//   console.log(newArr);
-// }
+//This function uses conditionals instead of for and if/else .
 
-// my code cannot access the substring this way 
-
-// + newArr[i].substr(1);
-
-//attempt # 2
-
-// jelly = "box jellyfish"
-// split into two words and invoke the function across all words
-// const newArr = jelly.split(" ").map(capitalizeFirstLetter);
-//join words with a space
-// const capitalizeJelly = newArr.join(" ");
-
-
-
-// problem 2 reverse a string
-
-// 1. make a string that will hold the value of the return string
-// function reverseAString(str) {
-
-//   let revStr = "";
-
-//   // step two break the string in a conditional loop that will start at -1 
-//   // the string begins at -1 - the last letter and will decrease through all letters 
-//   for (let i = str.length - 1; i >= 0; i--)
-//   // the reverse string is the string plus its reversed self
-//   newString = newString + str[i];
-//   return newString;
-// }
-
-// reverseAString("why god");
-
-
+// function to reverse
 function reverseString(str) {
-  // return a string that '?' if true return '' nothing else reverse string using substring and charAt
+  // return a string that '?' if equal to blank return blank otherwise ':' return reverse string using substring and charAt. 
   return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
 }
-// invoke function
+// invoke function - enter string in brackets
 let revStr = reverseString("My Life");
 
 // render on page
